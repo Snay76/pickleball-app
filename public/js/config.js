400877463dada968c56f6dac4357fd0cf11abfc2
Snay76 <yanscote@gmail.com>
2026-01-22 16:50:39 -0500

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,1 +1,9 @@
+export const SUPABASE_URL = "https://cehqaxtcfmgjajmmcccz.supabase.co";
+export const SUPABASE_ANON_KEY = "REMPLACE_PAR_TA_CLE";
 
+export const LS_ACCESS = "pb_access_token";
+export const LS_REFRESH = "pb_refresh_token";
+export const LS_EXPIRES_AT = "pb_expires_at";
+export const LS_VENUE_ID = "pb_venue_id";
+
+export const APP_VERSION = "2026-01-22-APP-REFAC-001";
