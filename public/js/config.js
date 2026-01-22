43edd0db8a8a8b876582b1ea82b495f9ce97d862
Snay76 <yanscote@gmail.com>
2026-01-22 16:55:36 -0500

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,5 @@
 export const SUPABASE_URL = "https://cehqaxtcfmgjajmmcccz.supabase.co";
-export const SUPABASE_ANON_KEY = "REMPLACE_PAR_TA_CLE";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlaHFheHRjZm1namFqbW1jY2N6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5NTQ1OTUsImV4cCI6MjA4NDUzMDU5NX0.U0IiTLx1_Vd4D8WBkhkO8KOrl3w_ftbPtcIWs_n1KmA";
 
 export const LS_ACCESS = "pb_access_token";
 export const LS_REFRESH = "pb_refresh_token";
