d62e78150d8c1c21fa95355160e41d14783b4da7
Snay76 <yanscote@gmail.com>
2026-01-23 15:43:20 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,5 @@
 import { APP_VERSION } from "./config.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("APP_VERSION =", APP_VERSION);
-
-  const el = document.getElementById("appVersion");
-  console.log("appVersion element =", el);
-
-  if (el) el.textContent = `v${APP_VERSION}`;
-});
+log("APP_VERSION = " + APP_VERSION);
 
 import { parseHash, saveTokens, refreshSessionIfNeeded, loadUser, sendMagicLink, hasSession, clearTokens } from "./auth.js";
 import { bindMainUI } from "./ui-main.js";
